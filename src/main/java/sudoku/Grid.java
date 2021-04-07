d8aa2c5f69f24a3123258d8922527defc64197cf
Lucas Braune <lucasbraune@gmail.com>
2021-04-07 16:21:09 -0300

    Add conversion between grids and strings

--- src/main/java/sudoku/Grid.java
+++ src/main/java/sudoku/Grid.java
@@ -11,6 +11,7 @@ import lombok.ToString;
 /**
  * A 9 x 9 sudoku grid.
  */
+@EqualsAndHashCode
 public class Grid {
 
     /**
@@ -25,10 +26,11 @@ public class Grid {
 
         @Getter
         private final int column;
-        
+
         /**
          * Constructs a coordinates on 9 x 9 grid from a specified pair of integers.
-         * @param row The first coordinate 
+         * 
+         * @param row    The first coordinate
          * @param column The second coordinate
          * @throws IndexOutOfBoundsException if either coordinate is < 0 or >= 9
          */
@@ -43,14 +45,24 @@ public class Grid {
     }
 
     public static enum Digit {
-        ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE;
+        ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9);
+
+        private final int intValue;
+
+        private Digit(int d) {
+            intValue = d;
+        }
+
+        public int intValue() {
+            return intValue;
+        }
     }
-    
+
     private final List<Optional<Digit>> data;
 
     public Grid() {
         data = new ArrayList<>();
-        for (int i=0; i<9*9; i++) {
+        for (int i = 0; i < 9 * 9; i++) {
             data.add(Optional.empty());
         }
     }
@@ -59,7 +71,7 @@ public class Grid {
         this.data = data;
     }
 
-    /** 
+    /**
      * Returns a deep copy of this grid.
      */
     @Override
@@ -72,14 +84,17 @@ public class Grid {
     }
 
     /**
-     * Returns the digit at the cell with the specified coordinates, if that cell is not blank.
+     * Returns the digit at the cell with the specified coordinates, if that cell is
+     * not blank.
      */
     public Optional<Digit> get(Coordinates coords) {
         return data.get(arrayIndex(coords));
     }
 
     /**
-     * Returns the digit at the cell with the specified coordinates, if that cell is not blank.
+     * Returns the digit at the cell with the specified coordinates, if that cell is
+     * not blank.
+     * 
      * @throws IndexOutOfBoundsException if either coordinate is < 0 or >= 9
      */
     public Optional<Digit> get(int row, int column) {
@@ -96,7 +111,7 @@ public class Grid {
         public GridOverwriteException(Grid grid, Coordinates coords) {
             super("Illegal attempt to overwrite grid.");
             this.coords = coords;
-            this.grid = grid; 
+            this.grid = grid;
         }
 
         /**
@@ -107,6 +122,7 @@ public class Grid {
 
     /**
      * Sets the digit at the cell with the specified coordinates.
+     * 
      * @throws GridOverwriteException if the cell is not blank
      */
     public void set(Coordinates coords, Digit d) {
@@ -115,14 +131,87 @@ public class Grid {
         }
         data.set(arrayIndex(coords), Optional.of(d));
     }
-    
+
     /**
      * Sets the digit at the cell with the specified coordinates.
-     * @throws GridOverwriteException if the cell is not blank
+     * 
+     * @throws GridOverwriteException    if the cell is not blank
      * @throws IndexOutOfBoundsException if either coordinate is < 0 or >= 9
      */
     public void set(int row, int column, Digit d) {
         set(new Coordinates(row, column), d);
     }
-    
+
+    private static char charFrom(Optional<Digit> d) {
+        return d.isPresent() ? Integer.toString(d.get().intValue()).charAt(0) : '0';
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int row=0; row < 9; row++) {
+            for (int column=0; column<9; column++) {
+                sb.append(charFrom(this.get(row, column)));
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    public static class GridParsingException extends Exception {
+        
+        public GridParsingException(String message) {
+            super(message);
+        }
+
+        /**
+         * See documentation of {@link java.io.Serializable}.
+         */
+        private static final long serialVersionUID = -5219699469780289049L;
+    }
+
+    private static Optional<Digit> optionalDigitFrom(char d) throws GridParsingException {
+        switch (d) {
+        case '0':
+            return Optional.empty();
+        case '1':
+            return Optional.of(Digit.ONE);
+        case '2':
+            return Optional.of(Digit.TWO);
+        case '3':
+            return Optional.of(Digit.THREE);
+        case '4':
+            return Optional.of(Digit.FOUR);
+        case '5':
+            return Optional.of(Digit.FIVE);
+        case '6':
+            return Optional.of(Digit.SIX);
+        case '7':
+            return Optional.of(Digit.SEVEN);
+        case '8':
+            return Optional.of(Digit.EIGHT);
+        case '9':
+            return Optional.of(Digit.NINE);
+        default:
+            throw new GridParsingException("Not a digit: " + d);
+        }
+    }
+
+    public static Grid from(String str) throws GridParsingException {
+        if (str.length() != 90) { // 9 rows * (9 digits + 1 new line character)
+            throw new GridParsingException("String of incorrect size: " + str.length());
+        }
+        Grid grid = new Grid();
+        for (int row=0; row<9; row++) {
+            for (int column=0; column<9; column++) {
+                int index = row * 10 + column;
+                Optional<Digit> d = optionalDigitFrom(str.charAt(index));
+                if (d.isPresent()) {
+                    grid.set(row, column, d.get());
+                }
+            }
+        }
+        return grid;
+    }
+
 }
--- src/test/java/sudoku/AppTest.java
+++ src/test/java/sudoku/AppTest.java
@@ -7,11 +7,14 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 import sudoku.Grid.Digit;
+import sudoku.Grid.GridOverwriteException;
+import sudoku.Grid.GridParsingException;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
+
     @Test
     public void gridStartsEmpty() {
         Grid grid = new Grid();
@@ -22,11 +25,11 @@ public class AppTest {
     public void getAfterSet() {
         Grid grid = new Grid();
         grid.set(0, 0, Digit.ONE);
-        assertEquals(Integer.valueOf(1), grid.get(0, 0).get());
+        assertEquals(Digit.ONE, grid.get(0, 0).get());
     }
 
     @Test
-    public void getWithBadIndicesThrowsException() {
+    public void badGridAccessThrowsException() {
         Grid grid = new Grid();
         assertThrows(IndexOutOfBoundsException.class, () -> { grid.get(-1, 0); });
         assertThrows(IndexOutOfBoundsException.class, () -> { grid.get(9, 0); });
@@ -35,11 +38,83 @@ public class AppTest {
     }
 
     @Test
-    public void settingCellTwiceThrowsException() {
+    public void gridOverwriteThrowsException() {
         Grid grid = new Grid();
-        assertThrows(IllegalArgumentException.class, () -> {
+        assertThrows(GridOverwriteException.class, () -> {
             grid.set(0, 0, Digit.ONE);
             grid.set(0, 0, Digit.TWO);
         });
+    }
+
+    private static Grid sampleGrid() {
+        Grid g = new Grid();
+        // 003020600
+        g.set(0, 2, Digit.THREE);
+        g.set(0, 4, Digit.TWO);
+        g.set(0, 6, Digit.SIX);
+        // 900305001
+        g.set(1, 0, Digit.NINE);
+        g.set(1, 3, Digit.THREE);
+        g.set(1, 5, Digit.FIVE);
+        g.set(1, 8, Digit.ONE);
+        // 001806400
+        g.set(2, 2, Digit.ONE);
+        g.set(2, 3, Digit.EIGHT);
+        g.set(2, 5, Digit.SIX);
+        g.set(2, 6, Digit.FOUR);
+        // 008102900
+        g.set(3, 2, Digit.EIGHT);
+        g.set(3, 3, Digit.ONE);
+        g.set(3, 5, Digit.TWO);
+        g.set(3, 6, Digit.NINE);
+        // 700000008
+        g.set(4, 0, Digit.SEVEN);
+        g.set(4, 8, Digit.EIGHT);
+        // 006708200
+        g.set(5, 2, Digit.SIX);
+        g.set(5, 3, Digit.SEVEN);
+        g.set(5, 5, Digit.EIGHT);
+        g.set(5, 6, Digit.TWO);
+        // 002609500
+        g.set(6, 2, Digit.TWO);
+        g.set(6, 3, Digit.SIX);
+        g.set(6, 5, Digit.NINE);
+        g.set(6, 6, Digit.FIVE);
+        // 800203009
+        g.set(7, 0, Digit.EIGHT);
+        g.set(7, 3, Digit.TWO);
+        g.set(7, 5, Digit.THREE);
+        g.set(7, 8, Digit.NINE);
+        // 005010300
+        g.set(8, 2, Digit.FIVE);
+        g.set(8, 4, Digit.ONE);
+        g.set(8, 6, Digit.THREE);
+        return g;
+    } 
+
+    private static final String sampleGridToString = 
+            "003020600\n" +
+            "900305001\n" +
+            "001806400\n" +
+            "008102900\n" +
+            "700000008\n" +
+            "006708200\n" +
+            "002609500\n" +
+            "800203009\n" +
+            "005010300\n";
+
+    @Test
+    public void gridToString() {
+        assertEquals(sampleGridToString, sampleGrid().toString());
+    }
+
+    @Test
+    public void gridParsingExceptionIsThrown() {
+        assertThrows(GridParsingException.class, () -> { Grid.from("Too short"); });
+    }
+
+    @Test
+    public void stringToGrid() throws GridParsingException {
+        assertEquals(sampleGrid(), Grid.from(sampleGridToString));
     }
 }
