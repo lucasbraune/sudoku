f285be42680d4aa9507d8a6a391638bcb9d3c31f
Lucas Braune <lucasbraune@gmail.com>
2021-04-12 22:38:42 -0300

    Remove unused methods from the Util class

--- src/main/java/sudoku/Util.java
+++ src/main/java/sudoku/Util.java
@@ -3,20 +3,12 @@ package sudoku;
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
-import java.util.ArrayList;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.function.Predicate;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 public class Util {
-
-    public static <T> Stream<T> makeStream(Iterable<T> iterable) {
-        return StreamSupport.stream(iterable.spliterator(), /* parallel? */ false);
-    }
 
     public static <T> Iterable<T> filter(Iterable<? extends T> iterable,
             Predicate<? super T> predicate) {
@@ -44,44 +36,6 @@ public class Util {
                 T nextValue = nextOpt.get();
                 nextOpt = Optional.empty();
                 return nextValue;
-            }
-
-        };
-    }
-
-    private static <T> List<Iterator<? extends T>> getIterators(
-            List<Iterable<? extends T>> iterables) {
-        List<Iterator<? extends T>> iterators = new ArrayList<>();
-        for (Iterable<? extends T> iterable : iterables) {
-            iterators.add(iterable.iterator());
-        }
-        return iterators;
-    }
-
-    public static <T> Iterable<T> concatenate(List<Iterable<? extends T>> iterables) {
-        return () -> new Iterator<T>() {
-            List<Iterator<? extends T>> iterators = getIterators(iterables);
-            Optional<T> nextOpt = Optional.empty();
-
-            @Override
-            public boolean hasNext() {
-                if (nextOpt.isPresent()) {
-                    return true;
-                }
-                for (Iterator<? extends T> it : iterators) {
-                    if (it.hasNext()) {
-                        nextOpt = Optional.of(it.next());
-                        return true;
-                    }
-                }
-                return false;
-            }
-
-            @Override
-            public T next() {
-                T nextVal = nextOpt.get();
-                nextOpt = Optional.empty();
-                return nextVal;
             }
 
         };
