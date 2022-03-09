b9e05eb92f6803f367c5faa50279e86872b2be7f
Lucas Braune <lucasbraune@gamil.com>
2022-03-10 00:47:50 +0100

    Run formatter

--- src/main/java/sudoku/AnnotatedGrid.java
+++ src/main/java/sudoku/AnnotatedGrid.java
@@ -16,17 +16,19 @@ import sudoku.GridElements.Column;
 import sudoku.GridElements.Box;
 
 /**
- * A grid with a set of digits associated to each of its empty cells. The set contains the candidate
- * values for that cell.
+ * A grid with a set of digits associated to each of its empty cells. The set
+ * contains the candidate values for that cell.
  * 
- * When a blank cell is set to a digit, that digit is ruled out as a cadidate in the blank cell's
- * row, column and box. Users of this class can also manually rule out a digit as a candidate for a
- * given cell, using its public {@code ruleOut} methods.
+ * When a blank cell is set to a digit, that digit is ruled out as a cadidate in
+ * the blank cell's row, column and box. Users of this class can also manually
+ * rule out a digit as a candidate for a given cell, using its public
+ * {@code ruleOut} methods.
  * 
- * This class overrides the {@code equals()} and {@code hashCode()} methods of its super class,
- * while preserving their contracts. An instance of {@code AnnotatedGrid} can only be equal to
- * other instances of {@code AnnotatedGrid}. Two instances of this class are equal if, and only
- * if, their underlying grids and sets of candidates are equal.
+ * This class overrides the {@code equals()} and {@code hashCode()} methods of
+ * its super class, while preserving their contracts. An instance of
+ * {@code AnnotatedGrid} can only be equal to other instances of
+ * {@code AnnotatedGrid}. Two instances of this class are equal if, and only if,
+ * their underlying grids and sets of candidates are equal.
  */
 @EqualsAndHashCode(callSuper = true)
 public final class AnnotatedGrid extends Grid {
@@ -43,8 +45,8 @@ public final class AnnotatedGrid extends Grid {
     }
 
     /**
-     * Creates a deep copy of the specified {@code AnnotatedGrid}. Both the underlying grid and
-     * candidate sets are copied.
+     * Creates a deep copy of the specified {@code AnnotatedGrid}. Both the
+     * underlying grid and candidate sets are copied.
      */
     public AnnotatedGrid(AnnotatedGrid grid) {
         super(grid);
@@ -52,7 +54,8 @@ public final class AnnotatedGrid extends Grid {
     }
 
     /**
-     * Removes the specified digit from the set of candidates for the specified cell.
+     * Removes the specified digit from the set of candidates for the specified
+     * cell.
      * 
      * @throws NoSuchElementException if the specified cell is not blank.
      */
@@ -65,8 +68,8 @@ public final class AnnotatedGrid extends Grid {
     }
 
     /**
-     * Rules out the specified digit as a candidate for the empty cells in the given range
-     * (typically a row, column or box).
+     * Rules out the specified digit as a candidate for the empty cells in the given
+     * range (typically a row, column or box).
      */
     public void ruleOut(Digit d, Iterable<Cell> cells) {
         for (Cell cell : emptyCells(cells)) {
@@ -75,8 +78,8 @@ public final class AnnotatedGrid extends Grid {
     }
 
     /**
-     * Sets the digit at specified empty cell. Rules out that digit as a candidate for the empty
-     * cells in the given cell's row, column and box.
+     * Sets the digit at specified empty cell. Rules out that digit as a candidate
+     * for the empty cells in the given cell's row, column and box.
      * 
      * @throws GridOverwriteException if the cell is not blank
      */
@@ -90,10 +93,11 @@ public final class AnnotatedGrid extends Grid {
     }
 
     /**
-     * Creates annotated from an ordinary grid. The annotated grid is a deep copy of the ordinary
-     * grid. The candidate sets are created as if by setting all digits as candidates for each
-     * empty cell, then ruling out the value of each nonempty cell as a candidate along thenonempty
-     * cell's row, column and box.
+     * Creates an annotated grid from an ordinary grid. The annotated grid is a deep
+     * copy of the ordinary grid. The candidate sets are created as if by setting
+     * all digits as candidates for each empty cell, then ruling out the value of
+     * each nonempty cell as a candidate along thenonempty cell's row, column and
+     * box.
      */
     public static AnnotatedGrid fromOrdinaryGrid(UnmodifiableGrid grid) {
         AnnotatedGrid annotatedGrid = new AnnotatedGrid();
@@ -113,10 +117,11 @@ public final class AnnotatedGrid extends Grid {
 
     /**
      * 
-     * Returns a set of candidates for value of the specified cell in a solution of this grid.
-     * This set is guaranteed to contain the value of this cell in every solution of this grid
-     * if the user of this grid never calls its {@code ruleOut} method manually (as opposed to
-     * automatically, when setting the value of a cell).
+     * Returns a set of candidates for value of the specified cell in a solution of
+     * this grid. This set is guaranteed to contain the value of this cell in every
+     * solution of this grid if the user of this grid never calls its
+     * {@code ruleOut} method manually (as opposed to automatically, when setting
+     * the value of a cell).
      * 
      * @throws NoSuchElementException if the specified cell is not empty
      */
@@ -128,10 +133,11 @@ public final class AnnotatedGrid extends Grid {
         }
     }
 
-    /** 
-     * Returns a string with one line for each empty cell. The lines are ordered according to the
-     * number of candidates for the corresponding cell; fewer candidates come first. Each line 
-     * contains the coordinates of its cell followed by its set of candidates.
+    /**
+     * Returns a string with one line for each empty cell. The lines are ordered
+     * according to the number of candidates for the corresponding cell; fewer
+     * candidates come first. Each line contains the coordinates of its cell
+     * followed by its set of candidates.
      */
     public final String candidatesToString() {
         List<Cell> cells = new ArrayList<>(emptyCells());
